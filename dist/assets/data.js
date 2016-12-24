2e170abfccc9dc37f01bc22279a725e04a569928
Andrea Giuffrida <Andrea@imac-di-andrea.home>
2016-12-24 16:01:22 +0100

    Dist updated

---
+++ dist/assets/data.js
@@ -0,0 +1,198 @@
+{
+  "name":"lin.bert website",
+  "manifest":[
+    {"id":"bert","src":"assets/about/bert.jpg"},
+    {"id":"wen","src":"assets/about/wen.jpg"},
+    {"id":"about","src":"assets/header/about.jpg"},
+    {"id":"kuafu","src":"assets/header/kuafu.jpg"},
+    {"id":"su","src":"assets/header/su.jpg"},
+    {"id":"dark-forest","src":"assets/header/dark-forest.jpg"},
+    {"id":"xmas-xperiment-2016","src":"assets/header/xmas-2016.jpg"},
+    {"id":"xmas-xperiment-2014","src":"assets/header/xmas-2014.jpg"},
+    {"id":"kuafu-01","src":"assets/kuafu/kuafu-01.jpg"},
+    {"id":"kuafu-02","src":"assets/kuafu/kuafu-02.jpg"},
+    {"id":"kuafu-03","src":"assets/kuafu/kuafu-03.jpg"},
+    {"id":"kuafu-04","src":"assets/kuafu/kuafu-04.jpg"},
+    {"id":"kuafu-05","src":"assets/kuafu/kuafu-05.jpg"},
+    {"id":"kuafu-06","src":"assets/kuafu/kuafu-06.jpg"},
+    {"id":"su-01","src":"assets/su/su-01.jpg"},
+    {"id":"su-02","src":"assets/su/su-02.jpg"},
+    {"id":"su-03","src":"assets/su/su-03.jpg"},
+    {"id":"su-04","src":"assets/su/su-04.jpg"},
+    {"id":"su-05","src":"assets/su/su-05.jpg"},
+    {"id":"su-06","src":"assets/su/su-06.jpg"},
+    {"id":"su-07","src":"assets/su/su-07.jpg"},
+    {"id":"su-08","src":"assets/su/su-08.jpg"},
+    {"id":"su-09","src":"assets/su/su-09.jpg"},
+    {"id":"su-10","src":"assets/su/su-10.jpg"},
+    {"id":"su-11","src":"assets/su/su-11.jpg"},
+    {"id":"dark-forest-01","src":"assets/dark-forest/dark-forest-01.jpg"},
+    {"id":"dark-forest-02","src":"assets/dark-forest/dark-forest-02.jpg"},
+    {"id":"dark-forest-03","src":"assets/dark-forest/dark-forest-03.jpg"},
+    {"id":"dark-forest-04","src":"assets/dark-forest/dark-forest-04.jpg"},
+    {"id":"dark-forest-05","src":"assets/dark-forest/dark-forest-05.jpg"},
+    {"id":"dark-forest-06","src":"assets/dark-forest/dark-forest-06.jpg"},
+    {"id":"dark-forest-07","src":"assets/dark-forest/dark-forest-07.jpg"},
+    {"id":"xmas-2016-01","src":"assets/xmas-2016/xmas-2016-01.jpg"},
+    {"id":"xmas-2016-02","src":"assets/xmas-2016/xmas-2016-02.jpg"},
+    {"id":"xmas-2016-03","src":"assets/xmas-2016/xmas-2016-03.jpg"},
+    {"id":"xmas-2016-04","src":"assets/xmas-2016/xmas-2016-04.jpg"},
+    {"id":"xmas-2014-01","src":"assets/xmas-2014/xmas-2014-01.jpg"},
+    {"id":"xmas-2014-02","src":"assets/xmas-2014/xmas-2014-02.jpg"},
+    {"id":"xmas-2014-03","src":"assets/xmas-2014/xmas-2014-03.jpg"}
+  ],
+  "projects":[
+    {
+      "name":"kuafu",
+      "route":"kuafu",
+      "type":"digital art installation",
+      "image":"kuafu",
+      "content": {
+        "title":"kuafu",
+        "contentL":"Kuafu is an interactive story based on the Chinese myth of the same name. It is the powerful story of a giant that one day decides to catch the sun and inevitably, fails to do so. Our interactive installation lets people experience the giant’s journey. We consider this project to be an experimentation and exploration of interactive storytelling. It's not a game but more an ‘interactive poem’, a universal story that speaks to everyone.",
+        "contentR":"In 2013 we entered it into the <a class=\"link link--kukuri\" data-letters=\"Google Devart\" href=\"https://devart.withgoogle.com/#/project/17570645\" target=\"_blank\">Google Devart</a> competition and ended up as finalists. Needless to say we were very proud of this result and have always wanted to finish the project. Time has past but the desire to create it is still intact, and we are happy to announce that Kuafu will be shown for the first time at the Awwward 2017 London Conference as a VR experience. Follow us to see the progress.",
+        "link":[
+          {
+            "label":"Watch the Video",
+            "href":"https://vimeo.com/191776687"
+          }
+        ],
+        "images":["kuafu-01,full","kuafu-02","kuafu-03","kuafu-04","kuafu-05","kuafu-06,full"]
+      }
+    },
+    {
+      "name":"su",
+      "route":"su",
+      "type":"interactive installation",
+      "image":"su",
+      "content": {
+        "title":"su",
+        "contentL":"Sù is an interactive installation in which you can explore the galaxy and it's constellations using leap motion. Through simple hand gestures, you can rotate the sky, point at specific constellations to zoom in and, with a clap of the hands, navigate to a random constellation. Throughout the last couple of years this installation has been exhibited three time in slight different forms.<h2>B-Reel 5th anniversary</h2>It was first set up for B-Reel’s 5th anniversary of the London office. For this occasion we built a 9ft dome in which we mapped out the night sky. During the evening, guests could draw their own constellation, name it and attach a message to it. Their creation was then immortalised in the installation. We also added sounds so that each interaction would trigger various musical notes, transforming the whole installation into a giant synthesizer.",
+        "contentR":"<h2 class=\"no-margin\">Stargazer International School of Geneva</h2>SÙ was part of a space themed event at the International School of Geneva, when we took 500 children for a journey through space. The installation was projected on a massive screen and each child had a chance to interact with the installation. Additionally they could draw their own constellation which was then projected during an evening event.<h2>OFF / DDD Milan 2016</h2>We were very honoured to be part of the first edition of the digital festival DDD Milan 2016, in which SÙ was displayed. For its third exhibition, we decided to create a relaxing experience. Guests could come and relax on big cushions while watching the stars and interact with the installation if they so wished.<br/>Thanks to all the people who have helped us make this project happen.",
+        "link":[],
+        "images":["su-01","su-02","su-03","su-04","su-05","su-06","su-07","su-08","su-09","su-10","su-11"]
+      }
+    },
+    {
+      "name":"dark forest",
+      "route":"dark-forest",
+      "type":"chrome experiment",
+      "image":"dark-forest",
+      "content": {
+        "title":"dark forest",
+        "contentL":"Dark forest is an experiment of the flashing behaviour of fireflies. Set in the darkness of the wood, the user can experience the life of the fireflies dancing in a 3D environment. Read more <a href=\"https://www.chromeexperiments.com/experiment/the-dark-forest \" target=\"_blank\" class=\"link link--kukuri\" data-letters=\"here\">here</a>.",
+        "contentR":"",
+        "link":[
+          {
+            "label":"Experience it",
+            "href":"https://www.chromeexperiments.com/experiment/the-dark-forest"
+          },
+          {
+            "label":"View the blog",
+            "href":"http://darkforest.bongiovi.tw/"
+          }
+        ],
+        "images":["dark-forest-01","dark-forest-02","dark-forest-03","dark-forest-04","dark-forest-05","dark-forest-06","dark-forest-07"]
+      }
+    },
+    {
+      "name":"xmas 2016",
+      "route":"xmas-2016",
+      "type":"chrome experiment",
+      "image":"xmas-xperiment-2016",
+      "content": {
+        "title":"xmas xperiment 2016",
+        "contentL":"We were very pleased to be invited once again to the Xmas Xperiment. For this edition we’ve decided to explore the visualisation of animals at night. Set in woods, the user can explore their surroundings and discover animals being created in space by a single flying light.<br />This project was created in collaboration with <a href=\"https://twitter.com/mad_clem\" target=\"_blank\" data-letters=\"Clément Chenebault\" class=\"link link--kukuri\">Clément Chenebault</a>.",
+        "contentR":"",
+        "link":[
+          {
+            "label":"Experience it",
+            "href":"http://christmasexperiments.com/2016/01/night-eye/"
+          }
+        ],
+        "images":["xmas-2016-01","xmas-2016-02","xmas-2016-03","xmas-2016-04"]
+      }
+    },
+    {
+      "name":"xmas 2014",
+      "route":"xmas-2014",
+      "type":"chrome experiment",
+      "image":"xmas-xperiment-2014",
+      "content": {
+        "title":"xmas xperiment 2014",
+        "contentL":"In 2014 we participated in the Xmas Xperiment in which users blew on gold dust to reveal illustrations.",
+        "contentR":"",
+        "link":[
+          {
+            "label":"Experience it",
+            "href":"http://www.bongiovi.tw/projects/christmas2014/"
+          },
+          {
+            "label":"View the blog",
+            "href":"http://blog.bongiovi.tw/blow-my-christmas-experiment-this-year/"
+          }
+        ],
+        "images":["xmas-2014-01,full","xmas-2014-02,full","xmas-2014-03,full"]
+      }
+    }
+  ],
+  "about": {
+    "title":"About lin.bert",
+    "description":"Lin.bert is a creative collective, created by visual artists Lin Yi-Wen and Bertrand Carrara. Their interdisciplinary work combines art, design, sound and code to create digital experiences, with a strong focus on storytelling and playfulness.<br/>Based in London and Geneva their work has been exhibited around Europe.",
+    "wen":{
+      "title":"Lin Yi-Wen",
+      "description":"Wen is a Creative Technologist during the  day and a digital artist at night. That’s when he is not looking after his two lovely kids. On the foreground of the digital exploration, he’s constantly experimenting and pushing boundaries.<br/><br/>Originally from Taiwan, he lives and works in London having previously lived in Paris for 5 years. Living abroad has definitely had a huge impact on Wen’s life. The cultural changes and the variety of talented people he has met and worked with continues to inspire his creative work. Wen’s eastern culture and background is one of his main sources of inspiration.",
+      "picture":"wen"
+    },
+    "bert":{
+      "title":"Bertrand Carrara",
+      "description":"Bert is an art-director and has worked in the digital field for more than a decade, mainly in London but also in New York and now in Geneva (@Buzzbrothers).<br/><br/>With a background in Art & Design he loves experimenting with every type of medium and mixing them together. He has found in the digital field a place where he can express his need to mix storytelling, illustration, motion and interaction.",
+      "picture":"bert"
+    },
+    "friends":[
+      {"name":"Alex Jenkins","url":"http://www.alex-jenkins.co.uk/"},
+      {"name":"Owen Hindley","url":"http://www.owenhindley.co.uk/"},
+      {"name":"Viggo Mörck","url":"http://www.viggomorck.com/"},
+      {"name":"Filippo Spiezia","url":"http://www.filippospiezia.com/"},
+      {"name":"Clément Chenebault","url":"https://twitter.com/mad_clem"}
+    ]
+  },
+  "press": [
+    {
+      "label":"<b>Kuafu</b> Digital art Installation — <b>Awwwards</b> Conference in London",
+      "sub":"February 2017"
+    },
+    {
+      "label":"<b>SÙ</b> Interactive installation — <b>DDD2016</b> Milan Design Conference",
+      "sub":"October 2016"
+    },
+    {
+      "label":"<b>SÙ</b> Interactive installation — <b>International School of Geneva</b>",
+      "sub":"April 2016"
+    },
+    {
+      "label":"<b>SÙ</b> Interactive installation named Star Canvas — <b>B-Reel</b> 5th anniversary party",
+      "sub":"April 2014"
+    },
+    {
+      "label":"<b>Dark Forest</b> Web Experiment — <b>Chrome Experiment</b>",
+      "sub":"April 2014"
+    },
+    {
+      "label":"<b>Kuafu</b> Digital art Installation  — Finalist at <b>Google Devart</b>",
+      "sub":"2013"
+    },
+    {
+      "label":"<span class=\"icon-link\"></span><a class=\"link link--kukuri\" href=\"https://www.chromeexperiments.com/experiment/particles-valley\" target=\"_blank\" data-letters=\"Particles Valley\"><b>Particles Valley</b></a>",
+      "sub":"Chrome Experiments"
+    },
+    {
+      "label":"<span class=\"icon-link\"></span><a class=\"link link--kukuri\" href=\"https://www.chromeexperiments.com/experiment/the-dark-forest\" target=\"_blank\" data-letters=\"Dark Forest\"><b>Dark Forest</b></a>",
+      "sub":"Chrome Experiments"
+    },
+    {
+      "label":"<span class=\"icon-link\"></span><a class=\"link link--kukuri\" href=\"https://www.chromeexperiments.com/experiment/blossom\" target=\"_blank\" data-letters=\"Blossom\"><b>Blossom</b></a>",
+      "sub":"Chrome Experiments"
+    }
+  ]
+}
